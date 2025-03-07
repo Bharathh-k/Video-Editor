298ae2c7664d8e165f94080ea755407d37d0f0e6
Bharath K <148028778+Bharathh-k@users.noreply.github.com>
2025-03-07 22:37:36 +0530

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -1,7 +1,7 @@
 from moviepy.editor import *
 start=input("enter the starting tinme in s")
 end=input("enter the endtime in s ")
-clip = VideoFileClip("C:\\Users\\Bharath K\\Documents\\video editor\\clip.mp4").subclip(start,end)
+clip = VideoFileClip("clip.mp4").subclip(start,end)
 txt_clip = TextClip("bharath",fontsize=70,color='white')   #error as not able to import *
 txt_clip = txt_clip.set_pos('center').set_duration(end-start)
 clip = CompositeVideoClip([clip, txt_clip])
